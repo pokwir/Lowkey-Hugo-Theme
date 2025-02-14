4f9e9cc72c2a7ebcdfedfd59adf39311f478ba0d
Patrick Okwir <s2599125@ed.ac.uk>
2025-02-13 22:26:07 -0500

    Update README.md

--- config/tailwind.config.js
+++ config/tailwind.config.js
@@ -19,9 +19,41 @@ module.exports = {
       fontFamily: {
         'sans': ['"Inter"', '-apple-system', 'BlinkMacSystemFont', 'avenir next', 'avenir', 'segoe ui', 'helvetica neue', 'helvetica', 'Cantarell', 'Ubuntu', 'roboto', 'noto', 'arial', 'sans-serif'],
       },
+      colors: {
+        obsidian: {
+          h1: '#bf616a',
+          h2: '#CE93D8',
+          h3: '#a3be8c',
+          bold: '#ebcb8b',
+          link: '#E0F2F1',
+        }
+      },
+      typography: {
+        DEFAULT: {
+          css: {
+            maxWidth: '65ch',
+            color: 'var(--text-normal)',
+            h1: {
+              color: 'var(--h1-color)',
+            },
+            h2: {
+              color: 'var(--h2-color)',
+            },
+            h3: {
+              color: 'var(--h3-color)',
+            },
+            strong: {
+              color: 'var(--bold-color)',
+            },
+            code: {
+              color: 'var(--text-normal)',
+              backgroundColor: 'var(--code-background)',
+            },
+          }
+        }
+      }
     },
   },
   plugins: [],
   variants: ['group-hover'],
-}
-
+};
